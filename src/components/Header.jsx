111df3748672361c9ccb4b1565df1c886fc9f96f
Turiano Romero <romero.guzman18@gmail.com>
2020-12-05 15:28:16 -0500

    creation of header

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import {useState} from 'react';
+
+const Header = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleClick = _ => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <div className="header">
+      <h1>React Hooks</h1>
+      <button type="button" onClick={handleClick}>
+        {
+          darkMode ? 'Dark Mode' : 'Light Mode'
+        }
+      </button>
+    </div>
+  );
+};
+
+export default Header;
